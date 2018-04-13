e3a0150dc71718ed849e3255f86197b49ef6d283
Alexandre Keruzec <alexandre.keruzec@gmail.com>
2018-04-13 14:34:59 +0200

    merging 2 lists

--- src/main/java/user/Main.java
+++ src/main/java/user/Main.java
@@ -8,36 +8,37 @@ import java.util.ListIterator;
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<User> users = new ArrayList();
+		ArrayList<User> users1 = new ArrayList();
+		ArrayList<User> users2 = new ArrayList();
 		// creating and adding users
 		User u1 = new User("John", 20, true);
-		users.add(u1);
+		users1.add(u1);
 		User u2 = new User("Pierre", 34, true);
-		users.add(u2);
+		users1.add(u2);
 		User u3 = new User("Jeanne", 22, false);
-		users.add(u3);
+		users1.add(u3);
 		User u4 = new User("Pierrette", 55, false);
-		users.add(u4);
+		users1.add(u4);
 		User u5 = new User("Auguste", 82, true);
-		users.add(u5);
+		users1.add(u5);
 		User u6 = new User("Germain", 93, true);
-		users.add(u6);
+		users2.add(u6);
 		User u7 = new User("Thomas", 12, true);
-		users.add(u7);
+		users2.add(u7);
 		User u8 = new User("Micheline", 78, false);
-		users.add(u8);
+		users2.add(u8);
 		User u9 = new User("Chloe", 14, false);
-		users.add(u9);
+		users2.add(u9);
 		User u10 = new User("Nathalie", 38, false);
-		users.add(u10);
+		users2.add(u10);
 		
 		// displaying the users
-		for(User user : users) {
+		for(User user : users1) {
 			System.out.println(user.toString() + "\n");
 		}
 		
 		// sorting the users by name
-		Collections.sort(users, new Comparator<User>() {
+		Collections.sort(users1, new Comparator<User>() {
 		    public int compare(User u1, User u2) {
 		        return u1.getName().compareTo(u2.getName());
 		    }
@@ -46,20 +47,32 @@ public class Main {
 		System.out.println("\nsorted alphabetically\n\n");
 		
 		// displaying the users sorted by names
-		for(User user : users) {
+		for(User user : users1) {
 			System.out.println(user.toString() + "\n");
 		}
 		
 		// removing some users
-		users.remove(8);
-		users.remove(7);
+		users1.remove(u2);
+		users1.remove(u4);
 		
 		System.out.println("\nafter removal of Pierre and Pierrette\n\n");
 		
 		// displaying the users with an iterator
-		ListIterator<User> iterator = users.listIterator();
+		ListIterator<User> iterator = users1.listIterator();
 		while(iterator.hasNext()) {
 			System.out.println(iterator.next().toString() + "\n");
+		}
+		
+		System.out.println("\nmerging with another list\n\n");
+		
+		// merging both arraylist in a new one
+		ArrayList<User> users = new ArrayList();
+		users.addAll(users1);
+		users.addAll(users2);
+		
+		// display the new list
+		for(User user : users) {
+			System.out.println(user.toString() + "\n");
 		}
 		
 	}
